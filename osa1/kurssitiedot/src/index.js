834a3631b4fdd3a864a08df3758b55428a376fdd
Jussi-Pekka Martikainen <jbmartik@gmail.com>
2019-03-24 20:35:28 +0200

    Tehtava 1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -9,15 +9,21 @@ const Header = (props) => (
 
 const Content = (props) => (
     <>
-        <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2} {props.exercises2}</p>
-        <p>{props.part3} {props.exercises3}</p>
+        <Part part={props.part1} exercises={props.exercises1} />
+        <Part part={props.part2} exercises={props.exercises2} />
+        <Part part={props.part3} exercises={props.exercises3} />
     </>
 )
 
 const Total = (props) => (
     <>
         <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+    </>
+)
+
+const Part = (props) => (
+    <>
+        <p>{props.part} {props.exercises}</p>
     </>
 )
 
