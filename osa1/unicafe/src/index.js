d348bde3e52ca2a59470c6484ee2d12287428c13
Jussi-Pekka Martikainen <jbmartik@gmail.com>
2019-04-07 16:24:36 +0300

    Tehtava 1.8

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const CountVotes = (good, neutral, bad) => (good+neutral+bad)
+const SumVotes = (good, neutral, bad) => ((1*good)+(0*neutral)+(-1*bad))
+const AvgVotes = (good, neutral, bad) => (SumVotes(good, neutral, bad)/CountVotes(good, neutral, bad))
+const PosPropVotes = (good, neutral, bad) => ((good/CountVotes(good, neutral, bad))*100)
+
 const Header = props => <h1>anna palautetta</h1>
 const Footer = props => <h1>statistiikka</h1>
 
@@ -9,6 +14,19 @@ const Button = ({ handleClick, text }) => (
       {text}
     </button>
 )
+
+const Statistics = (props) => (
+  <>
+  <Footer/>
+  <Stat text='hyvä' value={props.good}/><br/>
+  <Stat text='neutraali' value={props.neutral}/><br/>
+  <Stat text='huono' value={props.bad}/><br/>
+  <Stat text='yhteensä' value={CountVotes(props.good,props.neutral,props.bad)}/><br/>
+  <Stat text='keskiarvo' value={AvgVotes(props.good,props.neutral,props.bad)}/><br/>
+  <Stat text='positiivisia' value={PosPropVotes(props.good,props.neutral,props.bad)}/>%<br/>
+  </>
+)
+
 
 const Stat = ({text, value}) => (
     <>
@@ -22,24 +40,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const CountVotes = () => (good+neutral+bad)
-  const SumVotes = () => (good-bad)
-  const AvgVotes = () => (SumVotes()/CountVotes())
-  const PosPropVotes = () => ( (good/CountVotes())*100)
-
   return (
     <div>
       <Header/>
       <Button handleClick={() => setGood(good + 1)} text='hyvä'/>
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutraali'/>
       <Button handleClick={() => setBad(bad + 1)} text='huono'/>
-      <Footer/>
-      <Stat text='hyvä' value={good}/><br/>
-      <Stat text='neutraali' value={neutral}/><br/>
-      <Stat text='huono' value={bad}/><br/>
-      <Stat text='yhteensä' value={CountVotes()}/><br/>
-      <Stat text='keskiarvo' value={AvgVotes()}/><br/>
-      <Stat text='positiivisia' value={PosPropVotes()}/>%<br/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
